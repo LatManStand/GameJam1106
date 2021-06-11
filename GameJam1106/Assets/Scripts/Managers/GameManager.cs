783e0e181434e11b3f210e0c890501e99a7dd4d2
Jaime Ruiznavarro <jaimeruiznavarroambrona@gmail.com>
2021-06-12 00:46:29 +0200

    UI Changes

--- GameJam1106/Assets/Scripts/Managers/GameManager.cs
+++ GameJam1106/Assets/Scripts/Managers/GameManager.cs
@@ -11,7 +11,6 @@ public class GameManager : MonoBehaviour
         if (instance == null)
         {
             instance = this;
-            DontDestroyOnLoad(gameObject);
         }
         else
         {
---
+++ GameJam1106/Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+
+    private bool showingOptionsPanel = false;
+
+    public GameObject optionsPanel;
+
+
+    public void ShowOptionsPanel(){
+
+        if (showingOptionsPanel){
+
+            optionsPanel.SetActive(false);
+            showingOptionsPanel = false;
+
+        }else{
+
+            optionsPanel.SetActive(true);
+            showingOptionsPanel = true;
+
+        }
+
+
+    }
+
+    public void ExitGame(){
+
+        Application.Quit(); 
+
+    }
+
+
+
+}
